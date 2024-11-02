55467b9e315718a65f2faae77c24b4d831d71f20
WrestlerSquid <75701809+WrestlerSquid@users.noreply.github.com>
2024-11-02 00:10:48 -0400

    Sprint 1 Endurance

---
+++ Code.js
@@ -0,0 +1,9 @@
+async function startProgram() {
+    await roll(90, 255, 3.9);
+    await delay(2);
+    await roll(0, 255, 2.3);
+    await delay(2);
+    await roll(270, 255, 4);
+    await delay(2);
+    await roll(180, 255, 2.3);
+    }
